d3d7979f5529d2d7837ebe4bc20bf7cf29e05c74
Yannick Roffin <yroffin@gmail.com>
2024-09-09 22:37:11 +0200

    feat(initial): :fire: setup initial project

---
+++ src/main/java/org/roffin/lanterna/LanternaApplication.java
@@ -0,0 +1,13 @@
+package org.roffin.lanterna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanternaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanternaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/roffin/lanterna/LanternaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roffin.lanterna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanternaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
